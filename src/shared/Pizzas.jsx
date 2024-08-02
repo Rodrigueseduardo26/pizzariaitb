a8783d423a6347697de7016e12c18dd7e8684094
Rodrigueseduardo26 <duduropaz@gmail.com>
2024-08-02 19:34:33 -0300

    mudançass

--- src/shared/Pizzas.jsx
+++ src/shared/Pizzas.jsx
@@ -1,8 +1,8 @@
-import Margherita from "../assets/images/Margherita.png"
-import Pepperoni from "../assets/images/Pepperoni.png"
-import Quattro Formaggi from "../assets/images/Quattro Formaggi.png"
-import Hawaiana from "../assets/images/Hawaiana.png"
-import Calabresa from "../assets/images/Calabresa.png"
+import margherita from "../assets/images/margherita.png"
+import pepperoni from "../assets/images/pepperoni.png"
+import quattroformaggi from "../assets/images/quattroformaggi.png"
+import hawaiana from "../assets/images/hawaiana.png"
+import calabresa from "../assets/images/calabresa.png"
 
 const Pizzas = [ 
     {
@@ -10,34 +10,34 @@ const Pizzas = [
     "nome":"Margherita",
     "descrição":
     "Pizza clássica com molho de tomate, mozzarella e manjericão.",
-    "imagem": Margherita
+    "imagem": margherita
     },
     {
     "id": 2,
     "nome":"Pepperoni",
     "descrição":"Pizza com molho de tomate, mozzarella e fatias de pepperoni.",
-    "imagem": Pepperoni
+    "imagem": pepperoni
     },
     {
     "id": 3,
     "nome" :
     "Quattro Formaggi",
     "descrição":"Pizza com uma mistura de quatro queijos: mozzarella, gorgonzola, parmesão e fontina",
-    "imagem": Quattro Formaggi
+    "imagem": quattroformaggi
     },
     {
     "id": 4,
     "nome":"Hawaiana",
     "descrição":
     "Pizza com molho de tomate, mozzarella, presunto e abacaxi.",
-    "imagem": Hawaiana
+    "imagem": hawaiana
     },
     {
     "id": 5,
     "nome":
     "Calabresa",
     "descrição":"Pizza com molho de tomate, mozzarella e linguiça calabresa.",
-    "imagem": Calabresa
+    "imagem": calabresa
     },
 
 ]
