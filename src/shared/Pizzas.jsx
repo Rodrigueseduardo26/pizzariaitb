c3b4dba4384279e4fbdbf0055c244fe4b25524cc
Rodrigueseduardo26 <159132558+Rodrigueseduardo26@users.noreply.github.com>
2024-08-02 18:25:07 -0300

    alteração

---
+++ src/shared/Pizzas.jsx
@@ -0,0 +1,45 @@
+import mussarela from "../assets/images/mussarela.png"
+import calabresa from "../assets/images/calabresa.png"
+import atum from "../assets/images/atum.png"
+import portuguesa from "../assets/images/portuguesa.png"
+import frango from "../assets/images/frango.png"
+
+const Pizzas = [ 
+    {
+    "id": 1,
+    "nome":"Margherita",
+    "descrição":
+    "Pizza clássica com molho de tomate, mozzarella e manjericão.",
+    "imagem": Margherita
+    },
+    {
+    "id": 2,
+    "nome":"Pepperoni",
+    "descrição":"Pizza com molho de tomate, mozzarella e fatias de pepperoni.",
+    "imagem": Pepperoni
+    },
+    {
+    "id": 3,
+    "nome" :
+    "Quattro Formaggi",
+    "descrição":"Pizza com uma mistura de quatro queijos: mozzarella, gorgonzola, parmesão e fontina",
+    "imagem": Quattro Formaggi
+    },
+    {
+    "id": 4,
+    "nome":"Hawaiana",
+    "descrição":
+    "Pizza com molho de tomate, mozzarella, presunto e abacaxi.",
+    "imagem": Hawaiana
+    },
+    {
+    "id": 5,
+    "nome":
+    "Calabresa",
+    "descrição":"Pizza com molho de tomate, mozzarella e linguiça calabresa.",
+    "imagem": Calabresa
+    },
+
+]
+
+export defaut Pizzas;
